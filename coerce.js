5444888473ce9f21701cadb41db41b3001ef3c10
JAMES LEE <jamesdlee6203@gmail.com>
2016-06-22 16:17:40 -0500

    fixed indexing into arrays

--- coerce.js
+++ coerce.js
@@ -1,5 +1,5 @@
-var values = ["undefined", null, true, false, '""', "1.2",
-  "one", 0, -0, NaN, Infinity, -Infinity, 1, "{}", "[]",
+var values = [undefined, null, true, false, "", "1.2",
+  "one", 0, -0, NaN, Infinity, -Infinity, 1, {}, [],
   [12], ['something'], function(){}];
 var numValues = values.map(Number);
 var stringValues = values.map(String);
@@ -13,10 +13,10 @@ function fillTable() {
     var cell1 = row.insertCell(1);
     var cell2 = row.insertCell(2);
     var cell3 = row.insertCell(3);
-    var text0 = document.createTextNode(values[r]);
-    var text1 = document.createTextNode(numValues[r]);
-    var text2 = document.createTextNode(stringValues[r]);
-    var text3 = document.createTextNode(booleanValues[r]);
+    var text0 = document.createTextNode(values[r - 1]); // first row is label
+    var text1 = document.createTextNode(numValues[r - 1]);
+    var text2 = document.createTextNode(stringValues[r - 1]);
+    var text3 = document.createTextNode(booleanValues[r - 1]);
     cell0.appendChild(text0);
     cell1.appendChild(text1);
     cell2.appendChild(text2);
